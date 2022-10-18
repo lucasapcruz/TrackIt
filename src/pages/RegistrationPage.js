23860020717a422db4abb3063706f958b22986f1
Lucas Cruz <lucas.apcruz@gmail.com>
2022-10-18 00:59:28 -0300

    feat: registration logic

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,20 +1,39 @@
+import axios from "axios"
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
+import BASE_URL from "../baseUrl"
 import logo from "./logo.png"
 
 export default function RegistrationPage() {
+
+    const navigate = useNavigate()
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
     const [photo, setPhoto] = useState("")
 
+    function register(){
+        const userInfo ={
+            email:email,
+            name: name,
+            image: photo, 
+            password:password
+        }
+        const promise = axios.post(`${BASE_URL}/auth/sign-up`, userInfo)
+
+        promise.then(() => navigate("/"))
+
+        promise.catch(() => alert("Não foi possível registrar, tente novamente"))
+
+    }
+
     return (
         <Container>
             <img src={logo} alt="Logo from TrackIt" />
             <LoginForm>
-                <form>
+                <form onSubmit={register}>
                     <input name="email" type="email" placeholder="email" required onChange={e => setEmail(e.target.value)}/>
                     <input name="password" type="password" placeholder="senha" required onChange={e => setPassword(e.target.value)}/>
                     <input name="name" type="text" placeholder="nome" required onChange={e => setName(e.target.value)}/>
